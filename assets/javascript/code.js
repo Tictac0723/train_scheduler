870d7036fc41ba34606fde5cf23fe4eac10ece2a
Tictac0723 <taylorclark0723@gmail.com>
2017-06-07 21:25:53 -0500

    html

---
+++ assets/javascript/code.js
@@ -0,0 +1,11 @@
+ var config = {
+    apiKey: "AIzaSyBpLY9KNCjR4fSF8MBtwS7KjZPw1M8_gdU",
+    authDomain: "train-scheduler-ea318.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ea318.firebaseio.com",
+    projectId: "train-scheduler-ea318",
+    storageBucket: "train-scheduler-ea318.appspot.com",
+    messagingSenderId: "777883440477"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
